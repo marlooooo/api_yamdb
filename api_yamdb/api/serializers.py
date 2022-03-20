59681c91b1bfca1245628550c1bc66d8aefa3024
Дмитрий Кутин <TacoKong@yandex.ru>
2022-03-20 22:21:46 +0300

    Наброски сериализаторов

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 from rest_framework import serializers
 
-from reviews import models 
+from reviews import models
 
 # Доступ к моделям через apps.get_model(app_label='review', model_name='User')
 
@@ -24,6 +24,32 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
 
+class TitleSerializer(serializers.ModelSerializer):
+    description = serializers.CharField(required=False)
+    genre = serializers.PrimaryKeyRelatedField(
+        queryset=models.Genre.objects.all(),
+    )
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=models.Category.objects.all(),
+    )
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        model = models.Title
+
+
+class GenreSeiralizer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = models.Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = models.Category
+
+
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username',
@@ -31,6 +57,7 @@ class ReviewSerializer(serializers.ModelSerializer):
         default=serializers.CurrentUserDefault()
     )
     pub_date = serializers.DateTimeField(read_only=True)
+
     class Meta:
         fields = ('id', 'text', 'author', 'score', 'pub_date')
         model = models.Review
