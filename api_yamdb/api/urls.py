a07079144333e86dc0f7905e2946e47484a3a801
Дмитрий Кутин <TacoKong@yandex.ru>
2022-03-20 22:16:39 +0300

    Добавил роутеров

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,6 +4,9 @@ from rest_framework import routers
 from . import views
 
 router_v1 = routers.DefaultRouter()
+router_v1.register(r'titles', views.TitleViewSet, 'titles')
+router_v1.register(r'genres', views.GenreViewSet, 'genres')
+router_v1.register(r'categories', views.CategoryViewSet, 'categories')
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     views.ReviewViewSet,
