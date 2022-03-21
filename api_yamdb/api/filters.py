42ff7b1e631e7d5e9934a8ec8f5b59ec93a8b9e3
Дмитрий Кутин <TacoKong@yandex.ru>
2022-03-21 23:38:59 +0300

    починка фильтров

---
+++ api_yamdb/api/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework
+import django_filters
+from reviews import models
+
+
+class TitleFilter(django_filters.FilterSet):
+    genre = rest_framework.CharFilter(field_name='genre__slug')
+    name = rest_framework.CharFilter(lookup_expr='contains', field_name='name')
+    category = rest_framework.CharFilter(field_name='category__slug')
+
+    class Meta:
+        model = models.Title
+        fields = ('genre__slug', 'name', 'category__slug', 'year')
