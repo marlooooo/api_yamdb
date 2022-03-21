830db9fe840a9883126484c9babc241f627a924d
Дмитрий Кутин <TacoKong@yandex.ru>
2022-03-21 23:39:53 +0300

    Добавлен AdminReview для тестов

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Title
+from .models import Title, Review
 
 
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
     pass
 
 # Register your models here.
