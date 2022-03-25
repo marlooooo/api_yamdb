d2c519ec9960cb21a63ccd6c24b9b098254e8094
vlada <narl0dunr0@ya.ru>
2022-03-25 17:19:44 +0300

    Небольшие изменения по ревью(все кроме моделей, сериализаторов и вьюсетов)

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,9 +1,9 @@
-from django_filters import rest_framework
-import django_filters
+from django_filters import rest_framework, FilterSet
+
 from reviews import models
 
 
-class TitleFilter(django_filters.FilterSet):
+class TitleFilter(FilterSet):
     genre = rest_framework.CharFilter(field_name='genre__slug')
     name = rest_framework.CharFilter(lookup_expr='contains', field_name='name')
     category = rest_framework.CharFilter(field_name='category__slug')
